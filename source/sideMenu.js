cb7b6a33fa4b38a7240d74b0811c2fd4925885d0
Casey Jones <kcjones2002@gmail.com>
2016-03-15 20:56:08 -0700

    Add side menu

---
+++ source/sideMenu.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/* Requires react-native-side-menu to function*/
+
+import React from 'react-native';
+const window = React.Dimensions.get('window');
+
+
+export default class SideMenu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state.buttons = {
+            buttons: props.buttons
+        }
+    }
+
+    render() {
+
+        const menuButtons = this.state.buttons.map((btn, i) => {
+            return (<React.TouchableOpacity key={btn.text+i} activeOpacity={0.4}
+                    style={styles.button} onPress={btn.fn}>
+                        <React.Text style={styles.text}>{btn.text}</React.Text>
+                    </React.TouchableOpacity>
+                )
+        })
+
+        return (
+            <React.View style={styles.container}>
+                <React.View style={styles.menu}>
+                    {menuButtons}
+                </React.View>
+            </React.View>
+        )
+    }
+}
+
+const styles = React.StyleSheet.create({
+  container: {
+    width: window.width * (1/3),
+  },
+  menu: {
+    left: window.width * (1/3) * (1/6),
+    width: window.width * (1/3) * (2/3),
+    marginTop: React.Dimensions.get('window').height * 0.15,
+  },
+  button: {
+      borderColor: "black",
+      borderWidth: 1,
+      borderRadius: 4,
+      marginTop: 10,
+      padding: 5
+  },
+  text: {
+      textAlign: 'center',
+      fontSize: 22
+  }
+
+});
